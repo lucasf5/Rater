c268cc839c75dd6f50ca1fb86e92ed224be7ba36
Lucas Ferreira Pinto <lucasfpnt@gmail.com>
2024-08-08 21:09:00 -0300

    rater-02: [feat] creation theme and style global

---
+++ frontend/src/theme.ts
@@ -0,0 +1,28 @@
+const Theme = {
+  colors: {
+    accessibleText: {
+      11: "#B4B4B4",
+      12: "#EEEEEE",
+    },
+    background: {
+      1: "#121212",
+      2: "#191919",
+    },
+    interactive: {
+      3: "#232323",
+      4: "#2A2A2A",
+      5: "#313131",
+    },
+    separators: {
+      6: "#3A3A3A",
+      7: "#484848",
+      8: "#606060",
+    },
+    solid: {
+      9: "#7C7C7C",
+      10: "#6E6E6E",
+    },
+  },
+};
+
+export default Theme;
