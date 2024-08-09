7ccd13f5339edea68d4606b77022cc5dfccce46a
Lucas Ferreira Pinto <lucasfpnt@gmail.com>
2024-08-09 14:19:21 -0300

    rater-02: [feat] creation svgs and update globalstyles

--- frontend/src/GlobalStyles.ts
+++ frontend/src/GlobalStyles.ts
@@ -4,7 +4,7 @@ const GlobalStyles = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
-        font-family: sans-serif;
+        font-family: "Inter", sans-serif;
         background-color: ${(props) => props.theme.colors.background[1]};
         color: ${(props) => props.theme.colors.accessibleText[12]};
         padding: 1.5rem;
