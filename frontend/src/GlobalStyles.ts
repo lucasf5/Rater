a91720b76b6de352c64f84065ef3eed2cf837b94
Lucas Ferreira Pinto <lucasfpnt@gmail.com>
2024-08-12 11:36:39 -0300

    rater-02: [feat] update global styles

--- frontend/src/GlobalStyles.ts
+++ frontend/src/GlobalStyles.ts
@@ -8,6 +8,8 @@ const GlobalStyles = createGlobalStyle`
         background-color: ${(props) => props.theme.colors.background[1]};
         color: ${(props) => props.theme.colors.accessibleText[12]};
         padding: 1.5rem;
+        max-width: 1440px;
+        margin: 0 auto;
     }
     `;
 
